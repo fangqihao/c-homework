90d53fca45a49592937bb3dadc7ce1dcf289b18c
fangqihao <1398909559@qq.com>
2020-03-26 19:20:36 +0800

    求三个数的和

---
+++ p1-8/p1-9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  p1-9
+//
+//  Created by EDZ on 2020/3/26.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
