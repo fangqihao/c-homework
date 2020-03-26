60c45debb2c03de8d1e9115c293b72b9e352ad83
fangqihao <1398909559@qq.com>
2020-03-26 19:22:54 +0800

    x-code

---
+++ X-code/p1-9/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  p1-9
+//
+//  Created by EDZ on 2020/3/26.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a ,b, c ;
+          puts("请输入三个整数。");
+          
+          printf("整数1：");scanf ("%d",&a);
+          printf("整数2：");scanf ("%d",&b);
+          printf("整数3：");scanf ("%d",&c);
+    
+          printf("它们的和是%d\n",a + b + c);
+    return 0;
+}
