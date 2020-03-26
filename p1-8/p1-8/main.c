7d542f17009ac628ae403b8320d1987b6ede0e19
fangqihao <1398909559@qq.com>
2020-03-26 17:51:45 +0800

    练习1-8

---
+++ p1-8/p1-8/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  p1-8
+//
+//  Created by EDZ on 2020/3/26.
+//  Copyright © 2020 EDZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a ,b ;
+       puts("请输入两个整数。");
+       
+       printf("整数1：");scanf ("%d",&a);
+       printf("整数2：");scanf ("%d",&b);
+       
+       printf("它们的乘积是%d\n",a * b );
+    return 0;
+}
